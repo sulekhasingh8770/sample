858a17c48690aa99e2a90e9b505a7d68208d32ad
sulekhasingh8770 <48828405+sulekhasingh8770@users.noreply.github.com>
2019-05-24 18:25:49 +0530

    Delete QueryRepository.java

--- CurryPointSpringMvc/src/com/cg/currypoint/dao/QueryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cg.currypoint.dao;
-
-public interface QueryRepository {
-
-	public static final String FIND_BY_LOCATION="SELECT v FROM Vendor v, IN(v.address) a WHERE a.city= :city";
-	public static final String FIND_BY_NAME=" FROM Vendor v WHERE name=?1";
-}
